1bac8ea00c85d0fa453bfae5452a4f2fd67a449e
Maxat <maxat235@gmail.com>
2019-05-15 00:42:49 +0600

    added aut view

--- ITmeetups_back/api/views.py
+++ ITmeetups_back/api/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
 # Create your views here.
+
+
+class Login(ObtainAuthToken):
+
+    def post(self, request, *args, **kwargs):
+        global user
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key
+            # 'user_id': user.pk,
+            # 'email': user.email
+        })
